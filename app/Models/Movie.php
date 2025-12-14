636c4b837d32732ca5cf6a45777f6b8fc1e64c65
Geinki <gerrard.compton06@gmail.com>
2025-12-14 11:08:37 +0000

    Added category, movie and review models with relationship

---
+++ app/Models/Movie.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Movie extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'category_id',
+        'title',
+        'slug',
+        'release_year',
+        'imdb_id',
+        'poster',
+        'description',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function reviews()
+    {
+        return $this->hasMany(Review::class);
+    }
+}
